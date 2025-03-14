b93ad2ed5e4b5355865fcb6c9fe932eaf5133e51
Gabriel Gravena Barros <132004805+oppsggbarros@users.noreply.github.com>
2025-03-14 13:43:06 -0400

    Logica Tela login

---
+++ Farmacia/Back_End/Login/Login.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Login_Page
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Username=postgres;Password=2605;Database=AulaWindowsForm");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuario>().ToTable("usuario");
+        }
+    }
+        [Table("usuario")]
+    public class Usuario
+    {
+        [Column("id")]
+        [Key]
+        public int Id { get; set; }
+
+        [Column("nome")]
+        public string Nome { get; set; }
+        
+        [Column("senha")]
+        public string Senha { get; set; }
+    }
+}
