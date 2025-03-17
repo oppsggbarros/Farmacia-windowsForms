f7f2a2ad57e136493b930dfc516763bb00ac12b6
ArthurGuedes <arthurguedes0206@gmail.com>
2025-03-17 15:34:22 -0400

    CRUD_Usuarios

--- Farmacia/Back_End/CRUD/CRUD_Vendas.cs
+++ Farmacia/Back_End/CRUD/CRUD_Vendas.cs
@@ -51,40 +51,54 @@ namespace Farmacia.Back_End.CRUD
             catch (System.Exception ex)
             {
                 
-                throw;
+                MessageBox.Show(ex.Message);
+                return new List<Vendas>();
             }
         }
 
         
         public void Atualizar_Venda(int id, string cliente_nome, int medicamento_id, int quantidade, DateTime data_venda, decimal valor_total, int atendente_id)
         {
-            using (var con = new Conexao())
+            try 
             {
-                data_venda = data_venda.ToUniversalTime();
-                var venda = con.Vendas.Find(id);
-                if (venda != null)
+                
+                using (var con = new Conexao())
                 {
-                    venda.cliente_nome = cliente_nome;
-                    venda.medicamento_id = medicamento_id;
-                    venda.quantidade = quantidade;
-                    venda.data_venda = data_venda;
-                    venda.valor_total = valor_total;
-                    venda.atendente_id = atendente_id;
+                    data_venda = data_venda.ToUniversalTime();
+                    var venda = con.Vendas.Find(id);
+                    if (venda != null)
+                    {
+                        venda.cliente_nome = cliente_nome;
+                        venda.medicamento_id = medicamento_id;
+                        venda.quantidade = quantidade;
+                        venda.data_venda = data_venda;
+                        venda.valor_total = valor_total;
+                        venda.atendente_id = atendente_id;
 
-                    con.SaveChanges();
-                    Console.WriteLine("Venda atualizada com sucesso!");
+                        con.SaveChanges();
+                        Console.WriteLine("Venda atualizada com sucesso!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Venda não encontrada!");
+                    }
                 }
-                else
-                {
-                    Console.WriteLine("Venda não encontrada!");
-                }
+            
             }
+
+            catch (System.Exception ex)
+            {
+                
+                MessageBox.Show(ex.Message);
+            }   
         }
 
         
         public void Excluir_Venda(int id)
         {
-            using (var con = new Conexao())
+            try
+            {
+                using (var con = new Conexao())
             {
                 var venda = con.Vendas.Find(id);
                 if (venda != null)
@@ -97,6 +111,12 @@ namespace Farmacia.Back_End.CRUD
                 {
                     Console.WriteLine("Venda não encontrada!");
                 }
+            }
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                
             }
         }
 
