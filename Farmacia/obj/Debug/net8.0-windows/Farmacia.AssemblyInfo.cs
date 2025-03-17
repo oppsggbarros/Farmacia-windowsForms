7892febe739ddf313dfd6dff1954fbd463ea209a
Gabriel Gravena Barros <132004805+oppsggbarros@users.noreply.github.com>
2025-03-17 13:25:37 -0400

    teste gitignore

--- Farmacia/obj/Debug/net8.0-windows/Farmacia.AssemblyInfo.cs
+++ Farmacia/obj/Debug/net8.0-windows/Farmacia.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Farmacia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10e120b5e48779ad8c5b72f8bb63424cf5ed6bdd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58088cb9ffd428918ebc51edf4eacd981c9c05d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Farmacia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Farmacia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
